c764dc634412f42d7eb7987ba52369e6a559297a
Hannes Rudolph <hrudolph@gmail.com>
2025-10-15 09:42:25 -0600

    refactor: remove AnimatedBackground component from Layout (#378)

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
-import { AnimatedBackground } from '../../components/AnimatedBackground';
 import { CookieConsent } from '../../components/CookieConsent';
 import { PostHogProvider } from '../../components/PostHogProvider';
 import { TeamsBanner } from '../../components/TeamsBanner';
@@ -9,7 +8,6 @@ export default function Layout(props) {
   return (
     <PostHogProvider>
       <TeamsBanner />
-      <AnimatedBackground />
       <OriginalLayout {...props} />
       <CookieConsent />
     </PostHogProvider>
