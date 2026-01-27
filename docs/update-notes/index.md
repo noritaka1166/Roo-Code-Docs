33660c1312bd1acade0997c8f540cdc3ca0604a3
Hannes Rudolph <hrudolph@gmail.com>
2026-01-27 16:39:02 -0700

    docs: add v3.44 release notes (#513)

--- docs/update-notes/index.md
+++ docs/update-notes/index.md
@@ -21,6 +21,15 @@ If you want to live on the edge and try things out before it's released, we have
 
 ---
 
+### Version 3.44
+
+*   [3.44](/update-notes/v3.44) (Combined)
+*   [3.44.2](/update-notes/v3.44.2) (2026-01-27)
+*   [3.44.1](/update-notes/v3.44.1) (2026-01-27)
+*   [3.44.0](/update-notes/v3.44.0) (2026-01-27)
+
+---
+
 ### Version 3.43
 
 *   [3.43.0](/update-notes/v3.43.0) (2026-01-23)
--- sidebars.ts
+++ sidebars.ts
@@ -174,6 +174,16 @@ const sidebars: SidebarsConfig = {
             'update-notes/index',
             {
               type: 'category',
+              label: '3.44',
+              items: [
+                { type: 'doc', id: 'update-notes/v3.44', label: '3.44 Combined' },
+                { type: 'doc', id: 'update-notes/v3.44.2', label: '3.44.2' },
+                { type: 'doc', id: 'update-notes/v3.44.1', label: '3.44.1' },
+                { type: 'doc', id: 'update-notes/v3.44.0', label: '3.44.0' },
+              ],
+            },
+            {
+              type: 'category',
               label: '3.43',
               items: [{type: 'doc', id: 'update-notes/v3.43.0', label: '3.43.0'}],
             },
